74a5fa9b269c4e6b0ed081ccc1784049eaaad130
flo34k <146440052+flo34k@users.noreply.github.com>
2024-02-25 00:28:34 +0300

    fix the icon

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function refreshWeather(response) {
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
   }
   function formatDate(date) {
     let minutes = date.getMinutes();
