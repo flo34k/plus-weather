f5325d2ffe3645e687a6be7d5130df4da68f0a9b
flo34k <146440052+flo34k@users.noreply.github.com>
2024-02-24 22:48:37 +0300

    add axios and api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temprature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+  
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+  }
+
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#usersearchinput");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#usersearch");
